c1c9c054f5fe75e499f15cc86ccd4ce4472114f3
Sujal-D-88 <24kothales@rbunagpur.in>
2026-01-11 12:46:43 +0530

    First Commit of Official Site

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+        <ThemeProvider attribute="class" enableSystem disableTransitionOnChange>
           {children}
         </ThemeProvider>
       </body>
--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="relative bg-zinc-950 pt-20 pb-10 overflow-hidden border-t border-white/5">
+    <footer className="relative bg-zinc-950 dark:bg-zinc-50 pt-20 pb-10 overflow-hidden border-t border-white/5">
       
       {/* 1. Minute Detail: Subtle Background Grid for Texture */}
       <div className="absolute inset-0 bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)] pointer-events-none" />
